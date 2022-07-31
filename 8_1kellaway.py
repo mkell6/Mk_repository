0866391f771c4bdcd2b5afed7fce21c2f2e6ac91
Marissa <marissa@Marissas-MacBook-Pro.local>
2022-07-31 18:25:49 -0500

    first commit

---
+++ 8_1kellaway.py
@@ -0,0 +1,59 @@
+# This is a header for the application
+# You should read this header and insert your name and your date below as part of the peer review
+# This is a typical part of any program
+# Author: Marissa Kellaway
+# Creation Date: 31 July 2022
+# Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
+
+import random
+import time
+
+def displayIntro():
+	print('''You are in a land full of dragons. In front of you,
+	you see two caves. In one cave, the dragon is friendly
+	and will share his treasure with you. The other dragon
+	is greedy and hungry, and will eat you on sight.''')
+	print()
+
+def chooseCave():
+    cave = '' # I would change this to cave = input to be able to call later
+	while cave != '1' and cave != '2':
+		print('Which cave will you go into? (1 or 2)') # I encountered spacing problems here, switched from tabs to spaces
+		cave = input()
+
+	return caves #should be cave
+
+def checkCave(chosenCave):
+	print('You approach the cave...')
+	#sleep for 2 seconds
+	time.sleep(2)
+	print('It is dark and spooky...')
+	#sleep for 2 seconds
+	time.sleep(3)
+	print('A large dragon jumps out in front of you! He opens his jaws and...')
+	print()
+	#sleep for 2 seconds
+	time.sleep(2)
+	friendlyCave = random.randint(1, 2)
+
+	if chosenCave == str(friendlyCave):
+		print('Gives you his treasure!')
+	else:
+		print 'Gobbles you down in one bite!' #missing parantheses
+
+		#I inserted calls here:
+		#displayIntro()
+		#chooseCave()
+		#checkCave('cave')
+
+playAgain = 'yes' #I changed playAgain to =input, or else the program will keep running
+while playAgain = 'yes' or playAgain = 'y' #Should be ==
+	displayIntro()
+	caveNumber = choosecave() #  should be chooseCave()
+	checkCave(caveNumber)
+    
+	print('Do you want to play again? (yes or no)')
+	playAgain = input()
+	if playAgain == "no":
+		print("Thanks for planing") #spelling error: playing
+
